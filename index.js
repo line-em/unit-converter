3a7bf71c68f331276ac65fe0e66eafb959ae569b
Line-em <alineemily@hotmail.com>
2022-03-07 23:41:48 +0000

    Change syntax

--- index.js
+++ index.js
@@ -9,23 +9,30 @@ function metric() {
 	let metricMass = number / 2.2;
 	let metricVolume = number * 3.785;
 	let metricLength = number * 1.609;
-	mass.textContent = number + ' pounds is ' + metricMass.toFixed(3) + ' kg.';
-	volume.textContent = number + ' gallons is ' + metricVolume.toFixed(3) + ' litres.';
-	//length.textContent = number + ' miles is ' + metricLength.toFixed(3) + ' kms.';
-	length.textContent = `${number} miles is ${metricLength.toFixed(3)} kms.`;
+	mass.textContent = `${number} pounds is ${metricMass.toFixed(3)} kgs.`;
+	volume.textContent = `${number} gallons is ${metricVolume.toFixed(3)} litres.`;
+	length.textContent = `${number} miles is ${metricLength.toFixed(3)} km.`;
+
+	//mass.textContent = number + ' pounds is ' + metricMass.toFixed(3) + ' kg.';
+	//volume.textContent = number + ' gallons is ' + metricVolume.toFixed(3) + ' litres.';
+	//length.textContent = number + ' miles is ' + metricLength.toFixed(3) + ' km.';
 }
 
 function imperial() {
 	title.textContent = 'Imperial to Metric';
 	let number = document.getElementById('number').value;
-	let metricMass = number * 2.2;
-	let metricVolume = number / 3.785;
-	let metricLength = number / 1.609;
-	mass.textContent = number + ' kg is ' + metricMass.toFixed(3) + ' pounds.';
-	volume.textContent = number + ' litres is ' + metricVolume.toFixed(3) + ' gallons.';
-	length.textContent = number + ' kms is ' + metricLength.toFixed(3) + ' miles.';
+	let imperialMass = number * 2.2;
+	let imperialVolume = number / 3.785;
+	let imperialLength = number / 1.609;
+	mass.textContent = `${number} kg is ${imperialMass.toFixed(3)} pounds.`;
+	volume.textContent = `${number} litres is ${imperialVolume.toFixed(3)} gallons.`;
+	length.textContent = `${number} km is ${imperialLength.toFixed(3)} miles.`;
+
+	//mass.textContent = number + ' kg is ' + metricMass.toFixed(3) + ' pounds.';
+	//volume.textContent = number + ' litres is ' + metricVolume.toFixed(3) + ' gallons.';
+	//length.textContent = number + ' km is ' + metricLength.toFixed(3) + ' miles.';
 }
 
-//kmeter = mile, mile = kmeters
-//liters = gallons, gallons = litters
+//km = mile, mile = km
+//litres = gallons, gallons = litres
 //kg = pounds, pounds = kg
