aff7a3f83bb99facf0f812d96b21f77bc9eee650
Line-em <alineemily@hotmail.com>
2022-03-08 15:08:49 +0000

    Fixed some details in the files

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let length = document.getElementById('length');
-let volume = document.getElementById('volume');
-let mass = document.getElementById('mass');
-let title = document.getElementById('title');
+let length = document.getElementById("length");
+let volume = document.getElementById("volume");
+let mass = document.getElementById("mass");
+let title = document.getElementById("title");
 
 function metric() {
-	title.textContent = 'Metric to Imperial';
-	let number = document.getElementById('number').value;
+	title.textContent = "Metric to Imperial";
+	let number = document.getElementById("number").value;
 	let metricMass = number / 2.2;
 	let metricVolume = number * 3.785;
 	let metricLength = number * 1.609;
 	mass.textContent = `${number} pounds is ${metricMass.toFixed(3)} kgs.`;
-	volume.textContent = `${number} gallons is ${metricVolume.toFixed(3)} litres.`;
+	volume.textContent = `${number} gallons is ${metricVolume.toFixed(3)} liters.`;
 	length.textContent = `${number} miles is ${metricLength.toFixed(3)} km.`;
 
 	//mass.textContent = number + ' pounds is ' + metricMass.toFixed(3) + ' kg.';
@@ -19,13 +19,13 @@ function metric() {
 }
 
 function imperial() {
-	title.textContent = 'Imperial to Metric';
-	let number = document.getElementById('number').value;
+	title.textContent = "Imperial to Metric";
+	let number = document.getElementById("number").value;
 	let imperialMass = number * 2.2;
 	let imperialVolume = number / 3.785;
 	let imperialLength = number / 1.609;
 	mass.textContent = `${number} kg is ${imperialMass.toFixed(3)} pounds.`;
-	volume.textContent = `${number} litres is ${imperialVolume.toFixed(3)} gallons.`;
+	volume.textContent = `${number} liters is ${imperialVolume.toFixed(3)} gallons.`;
 	length.textContent = `${number} km is ${imperialLength.toFixed(3)} miles.`;
 
 	//mass.textContent = number + ' kg is ' + metricMass.toFixed(3) + ' pounds.';
